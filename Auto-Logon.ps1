67d544b1d4c6f675447726275a2018ff622850af
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-05-04 10:30:02 -0500

    Add files via upload

---
+++ Auto-Logon.ps1
@@ -0,0 +1,48 @@
+﻿function WouldYouLikeToSetupAutoLogin
+{
+    $TheirResponse = Read-Host -Prompt "Would you like to setup auto logon for this device? yes/no"
+
+    if ($TheirResponse -contains 'no')
+    {
+        write-host "Okay, Goodbye!"
+
+        Start-Sleep -seconds 3
+
+        exit
+    }
+    elseif ($TheirResponse -contains 'yes')
+    {
+        IsYourComputerOnADomain
+    }
+    else
+    {
+        WouldYouLikeToSetupAutoLogin
+    }
+}
+
+function IsYourComputerOnADomain
+{
+    
+}
+
+function EnterTheDomainName
+{
+
+}
+
+function EnterTheUserName
+{
+
+}
+
+function EnterThePassword
+{
+
+}
+
+function VerifyInput
+{
+
+}
+
+WouldYouLikeToSetupAutoLogin

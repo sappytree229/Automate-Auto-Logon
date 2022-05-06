9fbe6ab166a367dab52c9f0c33db8abed7ac48c5
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-05-05 22:10:02 -0500

    Update Auto-Logon.ps1

--- Auto-Logon.ps1
+++ Auto-Logon.ps1
@@ -1,6 +1,6 @@
 function WouldYouLikeToSetupAutoLogin()
 {
-    $TheirResponse = Read-Host -Prompt "Would you like to setup auto logon for this device? yes/no"
+    $TheirResponse = Read-Host "Would you like to setup auto logon for this device? yes/no"
 
     if ($TheirResponse -contains 'no')
     {
@@ -23,7 +23,7 @@ function WouldYouLikeToSetupAutoLogin()
 
 function IsYourComputerOnADomain()
 {
-    $OnDomain = Read-Host -Prompt "`nIs your computer on a domain? yes/no"
+    $OnDomain = Read-Host "`nIs your computer on a domain? yes/no"
 
     if ($OnDomain -ceq 'no')
     {
@@ -43,7 +43,7 @@ function IsYourComputerOnADomain()
 
 function EnterTheDomainName
 {
-    $DomainName = Read-Host -Prompt "`nWhat is the domain name?"
+    $DomainName = Read-Host "`nWhat is the domain name?"
 
     VerifyInput $DomainName {EnterTheDomainName}
 
@@ -55,7 +55,7 @@ function EnterTheDomainName
 
 function EnterTheUserName
 {
-    $Username = Read-Host -Prompt "`nEnter the username?"
+    $Username = Read-Host "`nEnter the username?"
 
     VerifyInput $Username {EnterTheUserName}
 
@@ -66,7 +66,7 @@ function EnterTheUserName
 
 function EnterThePassword
 {
-    $UserPassword = Read-Host -Prompt "Enter the user password" -AsSecureString
+    $UserPassword = Read-Host "Enter the user password" -AsSecureString
     $UserConfirmationPassword = Read-Host -Prompt "Confirm the user password" -AsSecureString
     $UserPasswordMatch = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($UserPassword))
     $UserConfirmationPasswordMatch = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($UserConfirmationPassword))
@@ -90,7 +90,7 @@ function EnterThePassword
 
 function VerifyInput ([string]$UserInput, [scriptblock]$FunctionToCall)
 {
-    $Confirmation = read-host -Prompt "`nIs " $UserInput " correct? yes/no"
+    $Confirmation = read-host "`nIs " $UserInput " correct? yes/no"
 
     if ($Confirmation -contains 'no')
     {
@@ -102,6 +102,11 @@ function VerifyInput ([string]$UserInput, [scriptblock]$FunctionToCall)
         
         start-sleep -Seconds 1
     }
+}
+
+function SetupAutoLogonRegistryKey()
+{
+
 }
 
 WouldYouLikeToSetupAutoLogin

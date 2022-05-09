a637100c209b3999b30db32126c8485b4dfb17f9
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-05-09 18:39:41 -0500

    Update Auto-Logon.ps1

--- Auto-Logon.ps1
+++ Auto-Logon.ps1
@@ -141,4 +141,11 @@ function VerifyKeysCreated()
     #Restart after verification
 }
 
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+    {
+        # Relaunch as an elevated process:
+        Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+        exit
+    }
+
 WouldYouLikeToSetupAutoLogin
